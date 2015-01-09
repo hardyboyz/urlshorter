a5c7fff47eaa60e0add6210b2a04786fb0ec0459
hardyboyz <hardi84@yahoo.com>
2015-01-09 19:35:19 +0800

    Update short_url.php

--- short_url.php
+++ short_url.php
@@ -3,10 +3,10 @@ if(isset($_POST['url'])){
 	require_once ("database_connection.php");
 	$url		= $_POST['url'];
 	$query		= "INSERT INTO url VALUES('','".$url."')";
-	$exec_query	= mysqli_query($koneksi,$query);
+	$exec_query	= mysqli_query($connect,$query);
 	
 	if($exec_query){
-		$data		= $config['url'].mysqli_insert_id($koneksi);
+		$data		= $config['url'].mysqli_insert_id($connect);
 		echo "Congratulations, Your URL has been shorted. <br />now you can access your url using <a href='".$data."' target='_blank'>".$data."</a>";
 	}else{
 		echo "Error";
